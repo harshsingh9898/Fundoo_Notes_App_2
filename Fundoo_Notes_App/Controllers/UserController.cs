71cbba11ea8d4bf55bd780f0b25efc6df664e47c
Harsh <harshtheuselessguy@gmail.com>
2022-08-04 09:09:25 +0530

    Add project files.

---
+++ Fundoo_Notes_App/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooNoteApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserBL iuserBL;
+
+        public UserController(IUserBL iuserBL)
+        {
+            this.iuserBL = iuserBL;
+        }
+
+
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult RegisterUser(UserRegistrationModel userRegistration)
+        {
+
+            try
+            {
+
+                var result = iuserBL.Registration(userRegistration);
+
+                if (result != null)
+                {
+                    return Ok(new { success = true, message = "Registration successful", data = result });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Registration unsuccessful" });
+                }
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+
+       
+
+
+
+
+    } } 
